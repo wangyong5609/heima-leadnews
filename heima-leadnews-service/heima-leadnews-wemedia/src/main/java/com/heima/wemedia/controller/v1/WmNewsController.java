278e450273a7431a4d5fe386ca4d9f9b123df6b8
YWang <1137917270@qq.com>
2023-10-17 21:08:55 +0800

    P50 自媒体-文章管理-文章列表查询

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmNewsController.java
@@ -0,0 +1,21 @@
+package com.heima.wemedia.controller.v1;
+
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
+import com.heima.wemedia.service.WmNewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/news")
+public class WmNewsController {
+
+    @Autowired
+    private WmNewsService WmNewsService;
+
+    @PostMapping("/list")
+    public ResponseResult findList(@RequestBody WmNewsPageReqDto dto) {
+        return WmNewsService.findList(dto);
+    }
+
+}
