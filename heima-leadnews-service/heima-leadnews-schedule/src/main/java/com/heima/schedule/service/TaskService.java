447a7b3ffb9dccd3b1aeff7dc82e8beddf317c28
YWang <1137917270@qq.com>
2023-10-30 21:24:51 +0800

    P86 延迟任务服务实现-添加任务

---
+++ heima-leadnews-service/heima-leadnews-schedule/src/main/java/com/heima/schedule/service/TaskService.java
@@ -0,0 +1,18 @@
+package com.heima.schedule.service;
+
+import com.heima.model.schedule.dtos.Task;
+
+/**
+ * 对外访问接口
+ */
+public interface TaskService {
+
+    /**
+     * 添加任务
+     *
+     * @param task 任务对象
+     * @return 任务id
+     */
+    long addTask(Task task);
+
+}
