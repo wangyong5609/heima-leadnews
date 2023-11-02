ed2f6a70083a76278505f5d8e276cf937c7ed112
YWang <1137917270@qq.com>
2023-11-02 21:16:59 +0800

    P95 发布文章添加延时任务

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
@@ -23,6 +23,7 @@ import com.heima.wemedia.mapper.WmNewsMapper;
 import com.heima.wemedia.mapper.WmNewsMaterialMapper;
 import com.heima.wemedia.service.WmNewsAutoScanService;
 import com.heima.wemedia.service.WmNewsService;
+import com.heima.wemedia.service.WmNewsTaskService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
@@ -80,6 +81,8 @@ public class WmNewsServiceImpl extends ServiceImpl<WmNewsMapper, WmNews> impleme
 
     @Autowired
     private WmNewsAutoScanService wmNewsAutoScanService;
+    @Autowired
+    private WmNewsTaskService wmNewsTaskService;
     /**
      * 发布修改文章或保存为草稿
      *
@@ -126,7 +129,8 @@ public class WmNewsServiceImpl extends ServiceImpl<WmNewsMapper, WmNews> impleme
         saveRelativeInfoForCover(dto, wmNews, materials);
 
         // 文章自动审核
-        wmNewsAutoScanService.autoScanWmNews(wmNews.getId());
+        // wmNewsAutoScanService.autoScanWmNews(wmNews.getId());
+        wmNewsTaskService.addNewsToTask(wmNews.getId(), wmNews.getPublishTime());
         return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
 
     }
---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsTaskServiceImpl.java
@@ -0,0 +1,38 @@
+package com.heima.wemedia.service.impl;
+
+import com.heima.apis.schedule.IScheduleClient;
+import com.heima.model.common.enums.TaskTypeEnum;
+import com.heima.model.schedule.dtos.Task;
+import com.heima.model.wemedia.pojos.WmNews;
+import com.heima.utils.common.ProtostuffUtil;
+import com.heima.wemedia.service.WmNewsTaskService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+@Slf4j
+public class WmNewsTaskServiceImpl implements WmNewsTaskService {
+    @Autowired
+    private IScheduleClient scheduleClient;
+    @Override
+    @Async
+    public void addNewsToTask(Integer id, Date publishTime) {
+        log.info("添加任务到延迟服务中----begin");
+
+        Task task = new Task();
+        task.setExecuteTime(publishTime.getTime());
+        task.setTaskType(TaskTypeEnum.NEWS_SCAN_TIME.getTaskType());
+        task.setPriority(TaskTypeEnum.NEWS_SCAN_TIME.getPriority());
+        WmNews wmNews = new WmNews();
+        wmNews.setId(id);
+        task.setParameters(ProtostuffUtil.serialize(wmNews));
+
+        scheduleClient.addTask(task);
+
+        log.info("添加任务到延迟服务中----end");
+    }
+}
