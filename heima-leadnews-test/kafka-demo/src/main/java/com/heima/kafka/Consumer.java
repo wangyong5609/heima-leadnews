6b7c6d999c3ad37e6f39de50939ed0ba8654a79d
YWang <1137917270@qq.com>
2023-11-04 20:32:12 +0800

    P107 Kafka生产者-手动提交偏移量

--- heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/Consumer.java
+++ heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/Consumer.java
@@ -1,12 +1,11 @@
 package com.heima.kafka;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
 
 import java.time.Duration;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 
 public class Consumer {
@@ -20,12 +19,43 @@ public class Consumer {
         //消息的反序列化器
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-
+        // 提交方式
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         //2.消费者对象
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
         //3.订阅主题
         consumer.subscribe(Collections.singletonList("itheima-topic"));
 
+        commitWithSyncAndAsync(consumer);
+    }
+
+    public static void commitWithSyncAndAsync(KafkaConsumer<String, String> consumer) {
+        try {
+            //当前线程一直处于监听状态
+            while (true) {
+                //4.获取消息
+                ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                    System.out.println(consumerRecord.key());
+                    System.out.println(consumerRecord.value());
+                    System.out.println(consumerRecord.offset());
+                    System.out.println(consumerRecord.partition());
+                    // 同步提交偏移量
+                    consumer.commitAsync();
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("异步提交失败" + e);
+        } finally {
+            try {
+                consumer.commitSync();
+            } finally {
+                consumer.close();
+            }
+        }
+    }
+
+    public static void syncCommit(KafkaConsumer<String, String> consumer) {
         //当前线程一直处于监听状态
         while (true) {
             //4.获取消息
@@ -33,7 +63,39 @@ public class Consumer {
             for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
                 System.out.println(consumerRecord.key());
                 System.out.println(consumerRecord.value());
+                System.out.println(consumerRecord.offset());
+                System.out.println(consumerRecord.partition());
+                // 同步提交偏移量
+                try {
+                    consumer.commitSync();
+                } catch (Exception e) {
+                    System.out.println("提交失败");
+                }
             }
         }
     }
+
+    public static void aSyncCommit(KafkaConsumer<String, String> consumer) {
+        while (true) {
+            //4.获取消息
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                System.out.println(consumerRecord.key());
+                System.out.println(consumerRecord.value());
+                System.out.println(consumerRecord.offset());
+                System.out.println(consumerRecord.partition());
+
+            }
+            // 异步提交
+            consumer.commitAsync(new OffsetCommitCallback() {
+                @Override
+                public void onComplete(Map<TopicPartition, OffsetAndMetadata> map, Exception e) {
+                    if (e != null) {
+                        System.out.println("异步提交失败偏移量：" + map + "异常" + e);
+                    }
+                }
+            });
+        }
+    }
+
 }
