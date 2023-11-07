6029506053c29f004a60469247a16c013f3a7969
YWang <1137917270@qq.com>
2023-11-07 21:32:06 +0800

    P126 搜索历史记录-保存搜索记录

--- heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/interceptor/AppTokenInterceptor.java
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/interceptor/AppTokenInterceptor.java
@@ -1,34 +1,28 @@
-package com.heima.wemedia.interceptor;
+package com.heima.search.interceptor;
 
-import com.heima.model.wemedia.pojos.WmUser;
-import com.heima.utils.thread.WmThreadLocalUtil;
+import com.heima.model.user.pojos.ApUser;
+import com.heima.utils.thread.AppThreadLocalUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class WmTokenInterceptor implements HandlerInterceptor {
+public class AppTokenInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String userId = request.getHeader("userId");
         if (StringUtils.isNoneBlank(userId)) {
             // 存入线程中
-            WmUser wmUser = new WmUser();
-            wmUser.setId(Integer.valueOf(userId));
-            WmThreadLocalUtil.setUser(wmUser);
+            ApUser apUser = new ApUser();
+            apUser.setId(Integer.valueOf(userId));
+            AppThreadLocalUtil.setUser(apUser);
         }
         return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        // WmThreadLocalUtil.clear();
-    }
-
-    @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        WmThreadLocalUtil.clear();
+        AppThreadLocalUtil.clear();
     }
 }
