5ac2d225e0440a87ec27f48515c768d6f353b400
YWang <1137917270@qq.com>
2023-11-08 21:14:44 +0800

    P128 搜索历史记录-删除搜索历史

--- heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/controller/v1/ApUserSearchController.java
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/controller/v1/ApUserSearchController.java
@@ -1,10 +1,12 @@
 package com.heima.search.controller.v1;
 
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.search.dtos.HistorySearchDto;
 import com.heima.search.service.ApUserSearchService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +29,8 @@ public class ApUserSearchController {
         return ResponseResult.okResult(apUserSearchService.findUserSearch());
     }
 
+    @PostMapping("/del")
+    public ResponseResult delUserSearch(@RequestBody HistorySearchDto historySearchDto) {
+        return apUserSearchService.delUserSearch(historySearchDto);
+    }
 }
--- heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/service/impl/ApUserSearchServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/service/impl/ApUserSearchServiceImpl.java
@@ -2,6 +2,7 @@ package com.heima.search.service.impl;
 
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.common.enums.AppHttpCodeEnum;
+import com.heima.model.search.dtos.HistorySearchDto;
 import com.heima.model.user.pojos.ApUser;
 import com.heima.search.pojos.ApUserSearch;
 import com.heima.search.service.ApUserSearchService;
@@ -77,5 +78,30 @@ public class ApUserSearchServiceImpl implements ApUserSearchService {
                 .with(Sort.by(Sort.Direction.DESC, "createdTime"));
         List<ApUserSearch> apUserSearches = mongoTemplate.find(query, ApUserSearch.class);
         return ResponseResult.okResult(apUserSearches);
+    }
+
+    /**
+     * 删除搜索历史
+     *
+     * @param historySearchDto
+     * @return
+     */
+    @Override
+    public ResponseResult delUserSearch(HistorySearchDto historySearchDto) {
+        //1.检查参数
+        if (historySearchDto.getId() == null) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+
+        //2.判断是否登录
+        ApUser user = AppThreadLocalUtil.getUser();
+        if (user == null) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
+        }
+
+        //3.删除
+        Query query = Query.query(Criteria.where("userId").is(user.getId()).and("id").is(historySearchDto.getId()));
+        mongoTemplate.remove(query, ApUserSearch.class);
+        return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
     }
 }
