467d6862320879e8e2802b6d7d7e2a03a8f3b6a7
YWang <1137917270@qq.com>
2023-11-11 17:55:38 +0800

    P130 平台管理-频道管理

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/WmChannelService.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/WmChannelService.java
@@ -2,9 +2,22 @@ package com.heima.wemedia.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.ChannelDto;
 import com.heima.model.wemedia.pojos.WmChannel;
 
 public interface WmChannelService extends IService<WmChannel> {
 
     ResponseResult findAll();
+
+    //保存
+    ResponseResult insert(WmChannel wmChannel);
+
+    //查询 
+    ResponseResult findByNameAndPage(ChannelDto dto);
+
+    //修改
+    ResponseResult update(WmChannel wmChannel);
+
+    //删除
+    ResponseResult delete(Integer id);
 }
