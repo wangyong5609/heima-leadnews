72fc596338ce4d3a0102c98ac0917f7e6580a18c
YWang <1137917270@qq.com>
2023-11-12 11:46:18 +0800

    P146 热点文章计算-定时计算-缓存分支较高的文章

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/mapper/ApArticleMapper.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/mapper/ApArticleMapper.java
@@ -4,17 +4,27 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
 import java.util.List;
 
 @Mapper
 public interface ApArticleMapper extends BaseMapper<ApArticle> {
     /**
      * 加载文章列表
-     * 
+     *
      * @param dto
      * @param type 1 加载更多 2加载最新
      * @return
      */
     List<ApArticle> loadArticleList(ArticleHomeDto dto, Short type);
+
+    /**
+     * 查询最近5天发布的文章
+     *
+     * @param dayParam
+     * @return
+     */
+    List<ApArticle> findArticleListByLast5days(@Param("dayParam") Date dayParam);
 }
