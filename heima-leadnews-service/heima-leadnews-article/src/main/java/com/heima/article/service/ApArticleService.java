c8aac621645c613d2f5fae16dbcf76b8eaa3a579
YWang <1137917270@qq.com>
2023-11-12 13:47:52 +0800

    P149 App端首页文章加载接口改造

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
@@ -16,6 +16,16 @@ public interface ApArticleService extends IService<ApArticle> {
     ResponseResult load(ArticleHomeDto dto, Short type);
 
     /**
+     * 加载文章列表
+     *
+     * @param dto
+     * @param type      1 加载更多   2 加载最新
+     * @param firstPage true  是首页  flase 非首页
+     * @return
+     */
+    ResponseResult load2(ArticleHomeDto dto, Short type, boolean firstPage);
+
+    /**
      * 保存app端相关文章
      *
      * @param dto
