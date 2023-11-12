e978eacaaf2eef11eb7b0914a818702a4be697c0
YWang <1137917270@qq.com>
2023-11-12 11:04:00 +0800

    P141 xxl-job 集群下任务路由策略-分片广播

--- heima-leadnews-test/xxljob-demo/src/main/java/com/heima/xxljob/job/HelloJob.java
+++ heima-leadnews-test/xxljob-demo/src/main/java/com/heima/xxljob/job/HelloJob.java
@@ -1,18 +1,42 @@
 package com.heima.xxljob.job;
 
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class HelloJob {
 
     @Value("${server.port}")
     private String port;
-    
+
     @XxlJob("demoJobHandler")
     public void helloJob() {
         System.out.println("简单任务执行了。。。。" + port);
+
+    }
+
+    @XxlJob("shardingJobHandler")
+    public void shardingJob() {
+
+        int shardIndex = XxlJobHelper.getShardIndex();
+        int shardTotal = XxlJobHelper.getShardTotal();
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            list.add(i);
+        }
+
+        for (Integer i : list) {
+            if (i % shardTotal == shardIndex) {
+                System.out.println("当前任务 " + i + " 由分片 " + shardIndex + " 执行");
+            }
+        }
 
     }
 }
